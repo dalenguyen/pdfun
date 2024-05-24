07dfd500208362bba1346ce22dba3fd88d803d41
Dale Nguyen <dale@dalenguyen.me>
2024-05-24 16:14:11 -0400

    chore: migrate to Nx 19 & Angular 18 (#1)
    * chore: migrate to Nx 19 & Angular 18
    
    * add .env template

--- pdf/src/app/shared/components/newsletter/newsletter.component.ts
+++ pdf/src/app/shared/components/newsletter/newsletter.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { ChangeDetectionStrategy, Component } from '@angular/core'
 
 @Component({
   selector: 'pdf-newsletter',
@@ -23,9 +23,6 @@ import { CommonModule } from '@angular/common'
         >
           <div id="mc_embed_signup_scroll">
             <h2>Subscribe for updates!</h2>
-            <div class="indicates-required">
-              <span class="asterisk">*</span> indicates required
-            </div>
             <div class="mc-field-group">
               <label for="mce-EMAIL"
                 >Email Address <span class="asterisk">*</span></label
@@ -69,19 +66,6 @@ import { CommonModule } from '@angular/common'
                   class="button"
                   value="Subscribe"
                 />
-                <p style="margin: 0px auto;">
-                  <a
-                    href="http://eepurl.com/iQwEgM"
-                    title="Mailchimp - email marketing made easy and fun"
-                    ><span
-                      style="display: inline-block; background-color: transparent; border-radius: 4px;"
-                      ><img
-                        class="refferal_badge"
-                        src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg"
-                        alt="Intuit Mailchimp"
-                        style="width: 220px; height: 40px; display: flex; padding: 2px 0px; justify-content: center; align-items: center;" /></span
-                  ></a>
-                </p>
               </div>
             </div>
           </div>
@@ -114,4 +98,4 @@ import { CommonModule } from '@angular/common'
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NewsletterComponent {}
+export class NewsletterComponent { }
