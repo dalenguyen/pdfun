2e4d5c42f38e123caeaa04fde53f6ba3e3bc58f5
Dale Nguyen <dale@dalenguyen.me>
2024-05-29 20:39:28 -0400

    chore: add zoneless change detection (#9)

--- pdf/src/app/app.config.ts
+++ pdf/src/app/app.config.ts
@@ -1,20 +1,28 @@
 import { provideFileRouter } from '@analogjs/router'
 import { provideHttpClient, withFetch } from '@angular/common/http'
-import { ApplicationConfig } from '@angular/core'
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core'
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app'
 import { getFirestore, provideFirestore } from '@angular/fire/firestore'
 import { getStorage, provideStorage } from '@angular/fire/storage'
 import { provideClientHydration } from '@angular/platform-browser'
-import { provideAnimations } from '@angular/platform-browser/animations'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 
 const firebaseConfig = JSON.parse(import.meta.env['VITE_FIREBASE_CONFIG'])
 export const appConfig: ApplicationConfig = {
   providers: [
     provideFileRouter(),
-    provideClientHydration(),
     provideHttpClient(withFetch()),
-    provideAnimations(),
-
+    provideAnimationsAsync(),
+    // If Angular hasn't start yet,
+    // replay the events after hydration with withEventReplay()
+    provideClientHydration(),
+    // remove zone.js and only reply on
+    // markForCheck, signal... for change detection
+    // Sadly Angular/fire needs to be updated first
+    provideExperimentalZonelessChangeDetection(),
     // Firebase
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideStorage(() => getStorage()),
--- pdf/src/main.server.ts
+++ pdf/src/main.server.ts
@@ -1,23 +1,24 @@
-import 'zone.js/node';
-import '@angular/platform-server/init';
+// TODO: remove after @angular/fire supports zoneless
+import '@angular/platform-server/init'
+import 'zone.js/node'
 
-import { enableProdMode } from '@angular/core';
-import { renderApplication } from '@angular/platform-server';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core'
+import { bootstrapApplication } from '@angular/platform-browser'
+import { renderApplication } from '@angular/platform-server'
 
-import { AppComponent } from './app/app.component';
-import { config } from './app/app.config.server';
+import { AppComponent } from './app/app.component'
+import { config } from './app/app.config.server'
 
 if (import.meta.env.PROD) {
-  enableProdMode();
+  enableProdMode()
 }
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = () => bootstrapApplication(AppComponent, config)
 
 export default async function render(url: string, document: string) {
   const html = await renderApplication(bootstrap, {
     document,
     url,
-  });
-  return html;
+  })
+  return html
 }
