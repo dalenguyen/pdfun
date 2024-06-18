99661a47826dd77a74241c317bc4f76d4ca4ea92
Dale Nguyen <dale@dalenguyen.me>
2024-06-18 16:03:48 -0400

    fix: improve unit tests

--- pdf/src/app/shared/components/disclaimer/disclaimer.component.spec.ts
+++ pdf/src/app/shared/components/disclaimer/disclaimer.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { provideRouter } from '@angular/router'
+import { FIREBASE_AUTH } from '@pdfun/angular/firebase'
+import { AuthService } from '@pdfun/angular/services'
 import { DisclaimerComponent } from './disclaimer.component'
 
 describe('DisclaimerComponent', () => {
@@ -8,6 +11,19 @@ describe('DisclaimerComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DisclaimerComponent],
+      providers: [
+        provideRouter([]),
+        {
+          provide: AuthService,
+          useValue: {
+            isLoggedIn: vitest.fn(),
+          },
+        },
+        {
+          provide: FIREBASE_AUTH,
+          useValue: {},
+        },
+      ],
     }).compileComponents()
 
     fixture = TestBed.createComponent(DisclaimerComponent)
